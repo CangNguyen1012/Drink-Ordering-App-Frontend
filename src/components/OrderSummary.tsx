9c70571eb7d131cb65a2ac367edf354fd7ddc005
CangNguyen1012 <brunomessi933@gmail.com>
2024-09-24 13:56:18 +0700

    Add to Cart

---
+++ src/components/OrderSummary.tsx
@@ -0,0 +1,57 @@
+import { Store } from "@/types";
+import { CardContent, CardHeader, CardTitle } from "./ui/card";
+import { CartItem } from "@/pages/DetailPage";
+import { Badge } from "./ui/badge";
+import { Separator } from "./ui/separator";
+
+type Props = {
+  store: Store;
+  cartItems: CartItem[];
+};
+
+const OrderSummary = ({ store, cartItems }: Props) => {
+  const getTotalCost = () => {
+    const totalCost = cartItems.reduce(
+      (total, cartItem) => total + cartItem.price * cartItem.quantity,
+      0
+    );
+
+    const totalCostWithDelivery = totalCost + store.deliveryPrice;
+
+    return totalCostWithDelivery;
+  };
+
+  return (
+    <>
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold tracking-tight flex justify-between">
+          <span>Your Order</span>
+          <span>{getTotalCost()} VND</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-5">
+        {cartItems.map((item) => (
+          <div className="flex justify-between">
+            <span>
+              <Badge variant="outline" className="mr-2">
+                {item.quantity}
+              </Badge>
+              {item.name}
+            </span>
+            <span className="flex items-center gap-1">
+              {item.price * item.quantity} VND
+            </span>
+          </div>
+        ))}
+        <Separator />
+        <div className="flex justify-between">
+          <span>Delivery</span>
+          <span>{store.deliveryPrice} VND</span>
+        </div>
+        <Separator />
+      </CardContent>
+    </>
+  );
+};
+
+export default OrderSummary;
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,12 +1,60 @@
 import { useGetStores } from "@/api/StoreApi";
 import MenuItems from "@/components/MenuItem";
+import OrderSummary from "@/components/OrderSummary";
 import StoreInfo from "@/components/StoreInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Card } from "@/components/ui/card";
+import { MenuItem } from "@/types";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type CartItem = {
+  _id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
 
 const DetailPage = () => {
   const { storeId } = useParams();
   const { store, isLoading } = useGetStores(storeId);
+
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (menuItem: MenuItem) => {
+    setCartItems((prevCartItems) => {
+      const existingCartItem = prevCartItems.find(
+        (cartItem) => cartItem._id === menuItem._id
+      );
+
+      let updatedCartItems;
+
+      if (existingCartItem) {
+        updatedCartItems = prevCartItems.map((cartItem) =>
+          cartItem._id === menuItem._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        updatedCartItems = [
+          ...prevCartItems,
+          {
+            _id: menuItem._id,
+            name: menuItem.name,
+            price: menuItem.price,
+            quantity: 1,
+          },
+        ];
+      }
+
+      sessionStorage.setItem(
+        `cartItems-${storeId}`,
+        JSON.stringify(updatedCartItems)
+      );
+
+      return updatedCartItems;
+    });
+  };
 
   if (isLoading || !store) {
     return "Loading...";
@@ -25,8 +73,17 @@ const DetailPage = () => {
           <StoreInfo store={store} />
           <span className="text-2xl font-bold tracking-tight">Menu</span>
           {store.menuItems.map((menuItem) => (
-            <MenuItems menuItem={menuItem} />
+            <MenuItems
+              menuItem={menuItem}
+              addToCart={() => addToCart(menuItem)}
+            />
           ))}
+        </div>
+
+        <div>
+          <Card>
+            <OrderSummary store={store} cartItems={cartItems} />
+          </Card>
         </div>
       </div>
     </div>
